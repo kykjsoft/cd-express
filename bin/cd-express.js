59b37ace8b8a4d3482a4ad328e8b0d59cfbf7b47
zhuqiang <290353142@qq.com>
2020-05-13 17:26:04 +0800

    1.0.0

---
+++ bin/cd-express.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const path = require("path");
+const fs = require("fs");
+const cwd = process.cwd();
+const serve = require('../index');
+const arg = require("arg")
+const pkg = require("../package.json")
+
+const args = arg({
+    // Types
+    '--help':    Boolean,
+    '--debug':    Boolean,
+    '--version': Boolean,
+    '--open': Boolean,
+    '--verbose': arg.COUNT,   // Counts the number of times --verbose is passed
+    '--port':    Number,      // --port <number> or --port=<number>
+    // Aliases
+    '-p':        '--port',
+    '-v':        '--version',
+    '-h':        '--help',
+    '-o':        '--open',
+});
+
+if(args["--help"]){
+    console.log(`
+        cd-express -p 5000 
+    `)
+    return ;
+}
+if(args["--version"]){
+    console.log(pkg.pkg)
+    return ;
+}
+
+const debug = function(){
+    if(args["--debug"]){
+        console.log.apply(console,arguments);
+    }
+}
+
+debug(__dirname);
+debug(cwd);
+debug(process.argv);
+debug(process.argv0);
+ 
+debug(args);
+const getConfig = function(filename){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(filename, (err, data) => {
+            if(err && err.code == 'ENOENT'){
+                resolve({})
+                return;
+            }
+            else if(err){throw err;}
+            debug(path.join(cwd,filename));
+            debug(path.join(__dirname,filename));
+            resolve(require(path.join(filename)))
+        });
+    })
+}
+
+let filename = args["_"].length?args["_"][0]:"cd-express.js";
+debug(filename);
+getConfig(path.join(cwd,filename))
+.then(function(config){
+    if(args["--open"]){
+        config.open = config.open || {}
+        config.open.enabled = true;
+    }
+    if(args["--debug"]){
+        config.debug = true;
+    }
+    if(args["--port"]){
+        config.port = args["--port"];
+    }
+    serve(config);
+}).catch(function(err){
+    console.error(err);
+})
+return;
---
+++ index.js
@@ -0,0 +1,62 @@
+
+const path = require("path");
+const express = require('express');
+const cwd = process.cwd();
+const defaultConfig = {
+    port:3000,
+    debug:false,
+    static:{
+        "~":["./"],
+    },
+    open:{
+        enabled:true,
+        app: 'chrome',
+        url:"/"
+    }
+}
+
+let debug = false;
+const log = function(msg){
+    debug&&console.log(msg);
+}
+const serve = function(pconfig){
+    debug = pconfig && pconfig.debug;
+    log("传入");
+    log(pconfig);
+    config = Object.assign({},defaultConfig,pconfig);
+    if(pconfig.open){
+        config.open = Object.assign({},defaultConfig.open,pconfig.open);
+    }
+    log("合并后");
+    log(config);
+    let app = express();
+    for(var name in config.static){
+        let paths = config.static[name];
+        if(typeof paths == "string"){
+            paths = [paths];
+        }
+        paths.forEach(item=>{
+            if(name=="~"){
+                app.use(express.static(path.join(cwd,item)));
+            }else{
+                app.use(name,express.static(path.join(cwd,item)));
+            }
+            log("[static] "+name +" " + path.join(cwd,item));
+        })
+    }
+    return new Promise(resolve=>{
+        app.listen(config.port,function(){
+            console.log("[express-server] 服务已启动 "+'http://127.0.0.1:'+config.port)
+            if(config.open.enabled){
+                const open = require('open');
+                let url = path.join(config.port.toString(),config.open.url);
+                url = 'http://127.0.0.1:'+url;
+                log(url);
+                open(url,{app:config.open.app});
+                resolve();
+            }
+        });
+    })
+}
+
+module.exports = serve;
